2c253ad841441f2e7a4d79932f05c40768ef438e
qwassi <sulha_ps3@yahoo.com>
2016-10-14 14:08:59 +0000

    Finished bullet and enemy animations.

--- game.js
+++ game.js
@@ -1,15 +1,15 @@
-
-BasicGame.Game = function (game) {
+BasicGame.Game = function(game) {
 
 };
 
 BasicGame.Game.prototype = {
-    preload: function () {
+    preload: function() {
         this.load.image('sea', 'assets/sea.png');
         this.load.image('bullet', 'assets/bullet.png');
         this.load.spritesheet('greenEnemy', 'assets/enemy.png', 32, 32);
+        this.load.spritesheet('explosion', 'assets/explosion.png', 32, 32);
     },
-    create: function () {
+    create: function() {
 
         this.sea = this.add.tileSprite(0, 0, 800, 600, 'sea');
 
@@ -17,19 +17,34 @@ BasicGame.Game.prototype = {
         this.enemy.animations.add('fly', [0, 1, 2], 20, true);
         this.enemy.play('fly');
         this.enemy.anchor.setTo(0.5, 0.5);
+        this.physics.enable(this.enemy, Phaser.Physics.ARCADE);
 
         this.bullet = this.add.sprite(400, 300, 'bullet');
         this.bullet.anchor.setTo(0.5, 0.5);
         this.physics.enable(this.bullet, Phaser.Physics.ARCADE);
-        this.bullet.body.velocity.y -= 50;
+        this.bullet.body.velocity.y = -50;
 
     },
-    update: function () {
+    update: function() {
         //  Honestly, just about anything could go here. It's YOUR game after all. Eat your heart out!
-        
+
         this.sea.tilePosition.y += 0.2;
+        this.physics.arcade.overlap(this.bullet, this.enemy,
+            this.enemyHit, null, this);
     },
-    quitGame: function (pointer) {
+    enemyHit: function(bullet, enemy) {
+        bullet.kill();
+        enemy.kill();
+        var explosion = this.add.sprite(enemy.x, enemy.y, 'explosion');
+        explosion.anchor.setTo(0.5, 0.5);
+        explosion.animations.add('boom');
+        explosion.play('boom', 15, false, true);
+    },
+    render: function() {
+        // this.game.debug.body(this.bullet);
+        // this.game.debug.body(this.enemy);
+    },
+    quitGame: function(pointer) {
 
         //  Here you should destroy anything you no longer need.
         //  Stop music, delete sprites, purge caches, free resources, all that good stuff.
