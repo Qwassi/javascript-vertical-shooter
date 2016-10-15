edc6e0a64a613434143278149c0853737fd76fe7
qwassi <sulha_ps3@yahoo.com>
2016-10-15 15:05:30 +0000

    Add player movement

--- game.js
+++ game.js
@@ -3,26 +3,44 @@ BasicGame.Game = function(game) {
 };
 
 BasicGame.Game.prototype = {
+
     preload: function() {
         this.load.image('sea', 'assets/sea.png');
         this.load.image('bullet', 'assets/bullet.png');
         this.load.spritesheet('greenEnemy', 'assets/enemy.png', 32, 32);
         this.load.spritesheet('explosion', 'assets/explosion.png', 32, 32);
+        this.load.spritesheet('player', 'assets/player.png', 64, 64);
     },
     create: function() {
 
         this.sea = this.add.tileSprite(0, 0, 800, 600, 'sea');
 
+        //Initialize enemy
         this.enemy = this.add.sprite(400, 200, 'greenEnemy');
         this.enemy.animations.add('fly', [0, 1, 2], 20, true);
         this.enemy.play('fly');
         this.enemy.anchor.setTo(0.5, 0.5);
         this.physics.enable(this.enemy, Phaser.Physics.ARCADE);
 
+
+
+        //Initialize bullet
         this.bullet = this.add.sprite(400, 300, 'bullet');
         this.bullet.anchor.setTo(0.5, 0.5);
         this.physics.enable(this.bullet, Phaser.Physics.ARCADE);
         this.bullet.body.velocity.y = -50;
+
+        //Initialize player
+        this.player = this.add.sprite(400, 550, 'player');
+        this.player.animations.add('fly', [0, 1, 2], 20, true);
+        this.player.play('fly');
+        this.player.anchor.setTo(0.5, 0.5);
+        this.physics.enable(this.player, Phaser.Physics.ARCADE);
+        this.player.speed = 300;
+        this.player.body.collideWorldBounds = true;
+
+        //Get keyboard cursor arrows
+        this.cursors = this.input.keyboard.createCursorKeys();
 
     },
     update: function() {
@@ -31,6 +49,35 @@ BasicGame.Game.prototype = {
         this.sea.tilePosition.y += 0.2;
         this.physics.arcade.overlap(this.bullet, this.enemy,
             this.enemyHit, null, this);
+
+        //Ensure the player to stop when
+        //no button is pressed.
+        this.player.body.velocity.x = 0;
+        this.player.body.velocity.y = 0;
+
+        //Seperate the x axis and y axis control
+        //to enable diagonal movement
+
+        //X-axis control
+        if (this.cursors.left.isDown) {
+            this.player.body.velocity.x = -this.player.speed;
+        }
+        else if (this.cursors.right.isDown) {
+            this.player.body.velocity.x = this.player.speed;
+        }
+
+        //Y-axis control
+        if (this.cursors.up.isDown) {
+            this.player.body.velocity.y = -this.player.speed;
+        }
+        else if (this.cursors.down.isDown) {
+            this.player.body.velocity.y = this.player.speed;
+        }
+
+        if (this.input.activePointer.isDown &&
+            this.physics.arcade.distanceToPointer(this.player) > 15) {
+            this.physics.arcade.moveToPointer(this.player, this.player.speed);
+        }
     },
     enemyHit: function(bullet, enemy) {
         bullet.kill();
